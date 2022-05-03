00dde32ff520c46370d183ccfb303ef653dc2eb4
ao <abdullahosama15@gmail.com>
2022-05-03 02:14:20 +0200

    Matrix definition

--- Matrix.h
+++ Matrix.h
@@ -16,10 +16,31 @@ private:
 public:
     Matrix(int n, int m, int p);
     // Set element in matrix
-    void set(int i, int j, int k, elemType element);
+    void set(int i, int j, int k, const elemType& element);
     // Get element from matrix
     elemType get(int i, int j, int k);
 };
 
+template<typename elemType>
+Matrix<elemType>::Matrix(int n, int m, int p): container(n * m * p) {
+    firstDim = n;
+    secondDim = m;
+    thirdDim = p;
+}
+
+template<typename elemType>
+int Matrix<elemType>::flattenIndex(int i, int j, int k) {
+    return i * secondDim * thirdDim + j * secondDim + k ;
+}
+
+template<typename elemType>
+elemType Matrix<elemType>::get(int i, int j, int k) {
+    return container[flattenIndex(i, j, k)];
+}
+
+template<typename elemType>
+void Matrix<elemType>::set(int i, int j, int k, const elemType& element) {
+    container[flattenIndex(i, j, k)] = element;
+}
 
 #endif // MATRIX_3D
